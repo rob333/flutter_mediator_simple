ec959ef4fe18c8b613bff48706bd5cb10b189e43
Robert Powell <rob333.2005@gmail.com>
2023-04-08 13:24:13 +0800

    Update github action

--- test/flutter_mediator_simple_test.dart
+++ test/flutter_mediator_simple_test.dart
@@ -1,2 +1,79 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:flutter_mediator_simple/flutter_mediator_simple.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'test_main.dart';
+
+void main() {
+  testWidgets('Mediator Simple - int increments test',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.textContaining(': 0'), findsNWidgets(3));
+    expect(find.textContaining(': 1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.textContaining(': 0'), findsNWidgets(2));
+    expect(find.text('int1: 2'), findsOneWidget);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.textContaining(': 0'), findsNWidgets(2));
+    expect(find.text('int1: 3'), findsOneWidget);
+
+    /// tab2
+    await tester.tap(find.byIcon(Icons.beach_access_sharp));
+    await tester.pump();
+    expect(find.textContaining(': 0'), findsNWidgets(2));
+    expect(find.text('int1: 3'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.textContaining(': 0'), findsOneWidget);
+    expect(find.text('int2: 2'), findsOneWidget);
+    expect(find.text('int1: 3'), findsOneWidget);
+
+    /// tab3
+    await tester.tap(find.byIcon(Icons.brightness_5_sharp));
+    await tester.pump();
+    expect(find.textContaining(': 0'), findsOneWidget);
+    expect(find.text('int2: 2'), findsOneWidget);
+    expect(find.text('int1: 3'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.text('int3: 1'), findsOneWidget);
+    expect(find.text('int2: 2'), findsOneWidget);
+    expect(find.text('int1: 3'), findsOneWidget);
+
+    /// List Tab
+    ///
+    // goto list tab
+    // await tester.tap(find.byIcon(Icons.power_input_rounded));
+    // await tester.pump();
+    // // Verify that list starts empty.
+    // expect(find.byType(GridTile), findsNothing);
+
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+    // // Verify that list item incremented.
+    // expect(find.byType(GridTile), findsOneWidget);
+
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+    // // Verify that list item incremented.
+    // expect(find.byType(GridTile), findsNWidgets(2));
+  });
+}
---
+++ test/test_var.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mediator_simple/flutter_mediator_simple.dart';
+
+// Mediator Variable: int1
+final _int1 = 0.rx; // or `final _int1 = Rx(0);`
+int get int1 => _int1.value;
+set int1(value) => _int1.value = value;
+
+void incrementInt1() {
+  int1++;
+}
+
+/// int2
+final _int2 = 0.rx; // or `final _int2 = Rx(0);`
+int get int2 => _int2.value;
+set int2(value) => _int2.value = value;
+
+void incrementInt2() {
+  int2++;
+}
+
+/// int3
+final _int3 = Rx(0); // or `final _int3 = 0.rx;`
+int get int3 => _int3.value;
+set int3(value) => _int3.value = value;
+
+void incrementInt3() {
+  int3++;
+}
+
+/// List page
+// Mediator Variable: data
+final _data = <ListItem>[].rx; //or `RxList(<ListItem>[]);`
+List<ListItem> get data => _data.value;
+set data(List<ListItem> value) => _data.value = value;
+SubscriberFn get dataSubscribe => _data.subscribe;
+List<ListItem> get dataNotify => _data.notify;
+
+class ListItem {
+  const ListItem(this.item, this.units, this.color);
+
+  final String item;
+  final int units;
+  final Color color;
+}
+
+//* item data
+const int maxItems = 35;
+const int maxUnits = 100;
+const List<String> itemNames = [
+  'Pencil',
+  'Binder',
+  'Pen',
+  'Desk',
+  'Pen Set',
+];
+const List<Color> itemColors = [
+  Colors.pink,
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.lightGreen,
+  Colors.green,
+  Colors.blue,
+  Colors.indigo,
+  Colors.purple,
+];
+
+void updateListItem() {
+  final units = Random().nextInt(maxUnits) + 1;
+  final itemIdx = Random().nextInt(itemNames.length);
+  final itemName = itemNames[itemIdx];
+  final color = itemColors[Random().nextInt(itemColors.length)];
+  if (data.length >= maxItems) data.clear();
+
+  //* Make an update to the collection type mediator variable.
+  // data.add(ListItem(itemName, units, color));
+  _data.notify.add(ListItem(itemName, units, color)); // notify  to rebuild
+}
